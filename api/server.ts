8637e4d250bfdf579dd515b4d57dbdb4c9640a69
Camille <camille.legeay@gmail.com>
2020-07-18 14:29:10 +0200

    installation cors

--- api/server.ts
+++ api/server.ts
@@ -5,13 +5,9 @@ import { apiRouter } from './api-routes';
 require('dotenv').config()
 
 var app = express();
+const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
+app.use(cors());
 
-// CORS enabled with express/node-js : 
-app.use(function(req, res, next) {
-res.header('Access-Control-Allow-Origin', '*'); //ou avec "www.xyz.com" à la place de "*" en production
-res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-next(); });
 
 var jsonParser = bodyParser.json() ;
 
